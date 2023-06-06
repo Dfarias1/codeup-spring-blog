623186f6d8cc42376bebcbfc0a7214fb91a31d8c
Daniel <daniel.h.farias2@gmail.com>
2023-06-06 17:14:34 -0500

    update

--- src/main/java/com/codeup/codeupspringblog/controllers/TestController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/TestController.java
@@ -1,37 +1,74 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Park;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // tells the compiler that this will extend HttpServlet and set up our class to control different URI patterns
 @Controller
 public class TestController {
-    //GET request mapping for /test
+    // Get Request Mapping for /test
     @GetMapping("/test")
-    //set response content type to text/html
+    // Set Response content type to text/html
     @ResponseBody
-    // define method that wil return text/html
-    public String test(){
+    // define method that will return text/html
+    public String test() {
         return "Hello Kotlin!";
     }
+
+
     // /parks
     @GetMapping("/parks/{park}/{message}")
     @ResponseBody
-    public String parks(@PathVariable String park, @PathVariable String message){
-        //Path Variables
+    public String parks(@PathVariable String park, @PathVariable String message) {
         return "<h1>Welcome to " + park + "!</h1><p>" + message + "</p>";
     }
-    @GetMapping("/parks")
-    @ResponseBody
-    public String parks(){
-        return "Welcome to Jumanji!";
+
+    @GetMapping("/parks/{user}")
+    public String parks(@PathVariable String user, Model model) {
+
+        Park p1 = new Park("Banff National Park", "Banff");
+        Park p2 = new Park("Badlands National Park", "North Dakota");
+        Park p3 = new Park("White Sands National Park", "New Mexico");
+
+        List<Park> parks = new ArrayList<>();
+
+//        parks.add(p1);
+//        parks.add(p2);
+//        parks.add(p3);
+
+        model.addAttribute("user", user);
+        model.addAttribute("parks", parks);
+
+        return "parks/index";
     }
-    //math example
+
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
     }
 
-}
 
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String submitCohortForm(@RequestParam String cohort, Model model) {
+        model.addAttribute("cohort", cohort);
+        return "join";
+    }
+
+    @GetMapping("/randomStudent")
+    public String showStudent() {
+        return "randomStudent";
+
+    }
+
+}
--- src/main/java/com/codeup/codeupspringblog/models/Park.java
+++ src/main/java/com/codeup/codeupspringblog/models/Park.java
@@ -1,5 +1,6 @@
-package com.codeup.codeupspringblog;
+package com.codeup.codeupspringblog.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,18 @@ import lombok.Setter;
 @Getter
 // Create all setter methods for each field
 @Setter
+@Entity
+@Table (name="parks")
 public class Park {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 75)
     private String name;
+
+    @Column(nullable = false, length = 20)
     private String location;
+
+    public Park(String banffNationalPark, String banff) {
+    }
 }
