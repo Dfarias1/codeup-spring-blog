02728e331766a46001d294f6b8af5e0a6bd45cea
Daniel <daniel.h.farias2@gmail.com>
2023-06-06 17:24:58 -0500

    JPA-repositorties exercise complete

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,41 +1,60 @@
 package com.codeup.codeupspringblog.controllers;
 
 import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class PostController {
+
+    private final PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
-    public String viewPost(Model model){
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("first post", "this my first post."));
-        posts.add(new Post("second post", "this my second post."));
+
+    public String posts(Model model) {
+        List<Post> posts = postsDao.findAll();
         model.addAttribute("posts", posts);
+
         return "posts/index";
- }
+    }
+
+
     @GetMapping("/posts/{id}")
-    public String singlePost(@PathVariable long id){
 
+    public String post(@PathVariable Long id, Model model) {
+        Post p1 = new Post(1L,"YOOO", "This is my first post!");
+        model.addAttribute("title", p1.getTitle());
+        model.addAttribute("body", p1.getBody());
         return "posts/show";
- }
+    }
+
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String showPostForm(){
+    public String create() {
+        return "posts/create";
+    }
 
-        return "View the form for creating";
- }
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String submitNewPost(){
 
-        return "Create new posts";
- }
+    public String createPost(@RequestParam String title, String body, Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("body", body);
+        Post post = new Post(title, body);
+        postsDao.save(post);
+
+        return "redirect:/posts";
+    }
+
+
 }
--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -1,5 +1,6 @@
 package com.codeup.codeupspringblog.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name="posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    @Column(nullable = false)
     private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
 }
