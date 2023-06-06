025535df6656363bb26e209d2edccdcf8bae3162
Daniel <daniel.h.farias2@gmail.com>
2023-06-06 10:29:41 -0500

    view exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,31 +1,41 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-     return "posts index page";
+    public String viewPost(Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("first post", "this my first post."));
+        posts.add(new Post("second post", "this my second post."));
+        model.addAttribute("posts", posts);
+        return "posts/index";
  }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewPost(@PathVariable long id){
-     return "Viewing post with the ID of: " + id;
+    public String singlePost(@PathVariable long id){
+
+        return "posts/show";
  }
     @GetMapping("/posts/create")
     @ResponseBody
     public String showPostForm(){
-     return "View the form for creating";
+
+        return "View the form for creating";
  }
     @PostMapping("/posts/create")
     @ResponseBody
     public String submitNewPost(){
-     return "Create new posts";
+
+        return "Create new posts";
  }
 }
